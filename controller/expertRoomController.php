3cf2478ef69eb910bad406893f36087c9667a5ba
BeSketens <87710895+BeSketens@users.noreply.github.com>
2022-04-22 09:05:00 +0200

    Update expertRoomController.php

--- controller/expertRoomController.php
+++ controller/expertRoomController.php
@@ -12,7 +12,7 @@ class ExpertRoom {
     public function runScript()
     {
         if (!IS_CONNECTED || (isset($_SESSION['status']) && $_SESSION['status'] != 'expert')) {
-            header('Location: ' . DOMAIN . 'home');
+            header('Location: ' . DOMAIN);
             exit();
         }
 
